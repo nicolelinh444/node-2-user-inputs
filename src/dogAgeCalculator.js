0b960c26d37274d6dc5e80822ec9ec6f8991b1d0
nicolelinh444 <nicolelinh444@gmail.com>
2025-10-29 09:20:20 -0700

    added comments to dogAgeCalculator.js

--- src/dogAgeCalculator.js
+++ src/dogAgeCalculator.js
@@ -1,12 +1,18 @@
+// declared variables for dog name and age
 let dogName = process.argv[2];
 let dogAge = Number(process.argv[3]);
 
+// used if else statement 
+// if dog is one years old
 if (dogAge === 1) {
     console.log(`Your dog, ${dogName}, is ${dogAge} years old, but that's 15 years old in dog years!`);
+    // if dog is 2 years old
   } else if (dogAge === 2) {
     console.log(`Your dog ${dogName}, is ${dogAge} years old, but that's 24 years old in dog years!`);
+    // if dog is more than 2 years old
   } else if (dogAge > 2) {
     console.log(`Your dog ${dogName}, is ${dogAge} years old, but that's ${(dogAge - 2) * 5 + 24} years old in dog years!`);
+    // if else, error message
   } else {
     console.log("Please enter a number for the dog's age.");
   }
